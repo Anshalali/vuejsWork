55f434f5f6f68dac60a976a31f1a798cb4cf2c9a
Anshalali <anshalali73@gmail.com>
2024-01-01 19:53:54 +0500

    enh: i have created navbar in vuejs

---
+++ vuejs-work/src/components/todoHeader.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/Vue_Logo_Black.png" alt="">
+                <h1>Vue Todos</h1>
+                <ul class="nav-routes">
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/about">About</RouterLink>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: #f1f1f1;
+    nav {
+        display: flex;
+        align-items: center;
+        padding: 25px 16px;
+
+        .branding {
+            display: flex;
+            align-items: center;
+            width: 130%;
+            gap: 8px;
+            img {
+                max-width: 70px;
+            }
+
+            h1 {
+                font-size: 30px;
+            }
+        }
+
+        .nav-routes {
+            display: flex;
+            flex: 1;
+            justify-content: flex-end;
+            gap: 25px;
+            list-style: none;
+
+            a {
+                font-size: 18px;
+                font-weight: 500;
+                text-decoration: none;
+                color: #252525;
+            }
+        }
+    }
+}
+</style>
