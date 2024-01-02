7709b4d919b0bb71eafc5314078eb90e903826f0
Anshalali <anshalali73@gmail.com>
2024-01-02 12:43:43 +0500

    enh: create files for todo list in vuejs

--- vuejs-work/src/views/TodosView.vue
+++ vuejs-work/src/views/TodosView.vue
@@ -1,5 +1,25 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator />
+  </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  border: 1px solid red;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+    border: 1px dotted green;
+  }
+}
+</style>
